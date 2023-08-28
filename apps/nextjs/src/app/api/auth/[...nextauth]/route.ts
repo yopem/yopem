d1d9f8a24a78818323c1a8ab169a04a8b0e036be
karyanayandi <halo@yandi.me>
2023-08-28 13:35:05 +0700

    initial

---
+++ apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,3 @@
+export { GET, POST } from "@yopem/auth";
+
+export const runtime = "edge";
---
+++ apps/nextjs/src/utils/api.ts
@@ -0,0 +1,6 @@
+import { createTRPCReact } from "@trpc/react-query";
+import type { AppRouter } from "@yopem/api";
+
+export const api = createTRPCReact<AppRouter>();
+
+export { type RouterInputs, type RouterOutputs } from "@yopem/api";
