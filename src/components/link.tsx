87cabb1cdc9cb6e6db5bfc1090199f73a927129d
Karyana Yandi <karyana@yandi.me>
2025-06-02 07:48:34 +0700

    feat(components): add predictive prefetch to Link
    Enhance Link component with js.foresight to enable smart prefetching based on
    user hover prediction. This improves page load performance by intelligently
    prefetching links before user interaction.

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,12 +1,65 @@
+"use client"
+
 import * as React from "react"
 import NextLink, { type LinkProps as NextLinkProps } from "next/link"
+import { useRouter } from "next/navigation"
+import { ForesightManager, type ForesightRect } from "js.foresight"
 
-interface LinkProps
-  extends React.HTMLAttributes<HTMLAnchorElement>,
-    NextLinkProps {}
+interface LinkProps extends Omit<NextLinkProps, "prefetch"> {
+  children: React.ReactNode
+  className?: string
+  hitSlop?: number | ForesightRect
+  unregisterOnCallback?: boolean
+  name?: string
+}
 
 const Link: React.FC<LinkProps> = (props) => {
-  return <NextLink prefetch scroll={false} {...props} />
+  const {
+    children,
+    className,
+    hitSlop = 0,
+    unregisterOnCallback = true,
+    name = "",
+    ...rest
+  } = props
+
+  const [isTouchDevice, setIsTouchDevice] = React.useState(false)
+  const LinkRef = React.useRef<HTMLAnchorElement>(null)
+
+  const router = useRouter()
+
+  React.useEffect(() => {
+    if (!LinkRef.current) {
+      return
+    }
+
+    const { unregister, isTouchDevice } = ForesightManager.instance.register({
+      element: LinkRef.current,
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string
+      callback: () => router.prefetch(props.href.toString()),
+      hitSlop,
+      name,
+      unregisterOnCallback,
+    })
+
+    setIsTouchDevice(isTouchDevice)
+
+    return () => {
+      unregister()
+    }
+  }, [LinkRef, router, props.href, hitSlop, name, unregisterOnCallback])
+
+  return (
+    <NextLink
+      {...props}
+      prefetch={isTouchDevice}
+      ref={LinkRef}
+      className={className}
+      {...rest}
+    >
+      {children}
+    </NextLink>
+  )
 }
 
 export default Link
