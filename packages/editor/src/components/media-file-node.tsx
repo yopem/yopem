9532ac78ce9a65c93ac2c7e93d5b147bcac5be96
Karyana Yandi <karyana@yandi.me>
2026-07-21 03:49:52 +0700

    feat(editor): add media file node component
    Introduce `media-file-node` component for handling file elements.
    Includes support for captions and resizable provider integration.

---
+++ packages/editor/src/components/media-file-node.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import type { TFileElement } from "platejs"
+import type { PlateElementProps } from "platejs/react"
+
+import { useMediaState } from "@platejs/media/react"
+import { ResizableProvider } from "@platejs/resizable"
+import { IconFileUpload } from "@tabler/icons-react"
+import { PlateElement, useReadOnly, withHOC } from "platejs/react"
+
+import { cn } from "ui"
+
+import { Caption, CaptionTextarea } from "./caption"
+
+export const FileElement = withHOC(
+  ResizableProvider,
+  function FileElement(props: PlateElementProps<TFileElement>) {
+    const readOnly = useReadOnly()
+    const { name, unsafeUrl } = useMediaState()
+
+    return (
+      <PlateElement className="my-px rounded-sm" {...props}>
+        <a
+          className={cn(
+            "group hover:bg-muted relative m-0 flex cursor-pointer items-center rounded px-0.5 py-0.75",
+          )}
+          contentEditable={false}
+          download={name}
+          href={unsafeUrl}
+          rel="noopener noreferrer"
+          role="button"
+          target="_blank"
+        >
+          <div className={cn("flex items-center gap-1 p-1")}>
+            <IconFileUpload className="size-5" />
+            <div>{name}</div>
+          </div>
+
+          <Caption align="left">
+            <CaptionTextarea
+              className="text-left"
+              readOnly={readOnly}
+              placeholder="Write a caption..."
+            />
+          </Caption>
+        </a>
+        {props.children}
+      </PlateElement>
+    )
+  },
+)
