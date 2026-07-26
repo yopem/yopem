f415a197af3e56475d5b29c26d548d40d36f0237
Karyana Yandi <karyana@yandi.me>
2026-07-27 02:08:30 +0700

    feat(utils): add new formatting utilities and wildcard export
    Refactored package.json exports to use wildcard pattern. Added new
    utility functions: `formatCurrency` for currency formatting and
    `formatNumberToFixed` for number abbreviation.

---
+++ packages/utils/src/format-currency.ts
@@ -0,0 +1,6 @@
+export const formatCurrency = (amount: number, currency = "USD"): string => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+  }).format(amount)
+}
---
+++ packages/utils/src/format-number-to-fixed.ts
@@ -0,0 +1,9 @@
+export const formatNumberToFixed = (num: number): string => {
+  if (num >= 1000000) {
+    return `${(num / 1000000).toFixed(1)}M`
+  }
+  if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}k`
+  }
+  return num.toString()
+}
