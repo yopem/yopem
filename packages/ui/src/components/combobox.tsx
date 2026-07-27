e1d18e8329628dc866b862b0040eb5ba30201156
Karyana Yandi <karyana@yandi.me>
2026-07-28 00:51:22 +0700

    fix(combobox): correct class for inset positioning
    Adjusts the class from `start-px` to `inset-s-px` to fix incorrect
    positioning of the start addon in the combobox component.

--- packages/ui/src/components/combobox.tsx
+++ packages/ui/src/components/combobox.tsx
@@ -66,7 +66,7 @@ function ComboboxInput({
       {startAddon && (
         <div
           aria-hidden="true"
-          className="pointer-events-none absolute inset-y-0 start-px z-10 flex items-center ps-[calc(--spacing(3)-1px)] opacity-80 has-[+[data-size=sm]]:ps-[calc(--spacing(2.5)-1px)] [&_svg]:-mx-0.5 [&_svg:not([class*='size-'])]:size-4.5 sm:[&_svg:not([class*='size-'])]:size-4"
+          className="pointer-events-none absolute inset-y-0 inset-s-px z-10 flex items-center ps-[calc(--spacing(3)-1px)] opacity-80 has-[+[data-size=sm]]:ps-[calc(--spacing(2.5)-1px)] [&_svg]:-mx-0.5 [&_svg:not([class*='size-'])]:size-4.5 sm:[&_svg:not([class*='size-'])]:size-4"
           data-slot="combobox-start-addon"
         >
           {startAddon}
